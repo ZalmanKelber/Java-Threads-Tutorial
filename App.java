f4f1904ca94a5b8d267285623feb4be4330373a9
ZalmanKelber <62047666+ZalmanKelber@users.noreply.github.com>
2020-11-24 17:56:56 -0500

    updated Step 01

--- App.java
+++ App.java
@@ -1,19 +1,32 @@
+class MyTask {
+    public void executeTask() {
+        for (int i = 0; i < 2; i++) {
+            System.out.println("printing task no " + i + " on printer 2");
+        }
+    }
+}
+
 public class App {
+
+    private static MyTask myTask = new MyTask();
 
     //main thread
     public static void main(String[] args) {
         //following executed by main thread in a sequence
-        //Job1 -> Job2 -> Job3
+        //Job1 -> Job2 -> Job3 -> Job4
 
         //Job1
         System.out.println("Application Started");
 
         //Job2
-        for (int i = 0; i < 4; i++) {
-            System.out.println("executing task no " + i);
-        }
+        myTask.executeTask();
 
         //Job3
+        for (int i = 0; i < 4; i++) {
+            System.out.println("printing task no " + i + " on printer 1");
+        }
+
+        //Job4
         System.out.println("Application Finished");
     }
 }
